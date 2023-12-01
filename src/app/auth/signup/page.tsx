f2ed254f20a124aeb14e97a614796cc7a9dda136
juanpmachadob <juanpmachadob@gmail.com>
2023-11-30 19:58:37 -0500

    feat: add Signup page and SignupForm component

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,20 @@
+import SignupForm from "@/components/auth/SignupForm";
+import Link from "next/link";
+
+export default function SignUpPage() {
+  return (
+    <section className="w-full">
+      <SignupForm />
+
+      <p className="text-center text-sm font-semibold text-text-alternative">
+        Or continue with:
+      </p>
+      <div className="my-2 flex w-full flex-col gap-2"></div>
+      <span className="flex justify-center gap-2 text-sm">
+        <Link href="/auth/login" className="text-primary hover:underline">
+          Already have an account?
+        </Link>
+      </span>
+    </section>
+  );
+}
---
+++ src/components/auth/SignupForm.tsx
@@ -0,0 +1,54 @@
+"use client";
+import Input from "@/components/ui/inputs/Input";
+import { useForm } from "react-hook-form";
+
+type FormInputs = {
+  email: string;
+  password: string;
+};
+
+const SignupForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
+
+  const onSubmit = handleSubmit((data) => {});
+
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="my-8 flex w-full flex-col gap-2"
+      noValidate
+    >
+      <h1 className="mb-2 text-center font-semibold">Sign up to continue</h1>
+
+      <Input
+        autoFocus={true}
+        id="email"
+        type="email"
+        placeholder="Enter your email"
+        {...register("email", {
+          required: "Please enter and email address",
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "Email must be valid",
+          },
+        })}
+        error={errors.email}
+      />
+      <span role="" className="text-xs text-text-alternative">
+        By continuing, you will receive an email to create your account
+      </span>
+
+      <button
+        type="submit"
+        className="w-full rounded bg-primary p-2 font-semibold text-white outline-0 hover:brightness-90 active:brightness-50"
+      >
+        Sign up
+      </button>
+    </form>
+  );
+};
+export default SignupForm;
