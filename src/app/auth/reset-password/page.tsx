5a1de36831fd3725391d469b5e471f3d3f96e74e
juanpmachadob <juanpmachadob@gmail.com>
2023-11-30 20:06:42 -0500

    feat: add Reset Password page and ResetPasswordForm component

---
+++ src/app/auth/reset-password/page.tsx
@@ -0,0 +1,16 @@
+import ResetPasswordForm from "@/components/auth/ResetPasswordForm";
+import Link from "next/link";
+
+export default function ResetPasswordPage() {
+  return (
+    <section className="w-full">
+      <ResetPasswordForm />
+
+      <span className="flex justify-center gap-2 text-sm">
+        <Link href="/auth/login" className="text-primary hover:underline">
+          Return to log in
+        </Link>
+      </span>
+    </section>
+  );
+}
---
+++ src/components/auth/ResetPasswordForm.tsx
@@ -0,0 +1,56 @@
+"use client";
+import Input from "@/components/ui/inputs/Input";
+import { useForm } from "react-hook-form";
+
+type FormInputs = {
+  email: string;
+  password: string;
+};
+
+const ResetPasswordForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
+
+  const onSubmit = handleSubmit((data) => {});
+
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="my-8 flex w-full flex-col gap-2"
+      noValidate
+    >
+      <h1 className="mb-2 text-center font-semibold">Can&apos;t log in?</h1>
+
+      <div>
+        <label htmlFor="email" className="text-xs font-semibold text-text-alternative">
+          We&apos;ll send a recovery link to
+        </label>
+        <Input
+          autoFocus={true}
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          {...register("email", {
+            required: "Please enter and email address",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Email must be valid",
+            },
+          })}
+          error={errors.email}
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="w-full rounded bg-primary p-2 font-semibold text-white outline-0 hover:brightness-90 active:brightness-50"
+      >
+        Send recovery link
+      </button>
+    </form>
+  );
+};
+export default ResetPasswordForm;
