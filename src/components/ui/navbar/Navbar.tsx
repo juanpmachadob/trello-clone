573ce8f0d6727a65756b1390031d3fa1c77745f5
juanpmachadob <juanpmachadob@gmail.com>
2023-12-05 19:56:06 -0500

    feat: add link in Trello Logo to the Boards page

--- src/components/ui/navbar/Navbar.tsx
+++ src/components/ui/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import clsx from "clsx";
 import { IoApps } from "react-icons/io5";
 import "./navbar.css";
@@ -12,20 +13,29 @@ export const Navbar = ({ isDarkContent, className }: Props) => {
   return (
     <header
       className={clsx(
-        "flex h-12 w-full flex-row items-center gap-4 border-b px-4",
+        "flex h-12 w-full flex-row items-center gap-2 border-b px-4",
         { "text-white border-b-white/15": !isDarkContent },
         { "text-text-alternative border-b-black/15": isDarkContent },
         className
       )}
     >
       <IoApps />
-      <Image
-        src="/assets/logos/trello-animated.gif"
-        alt="Trello logo"
-        className={clsx({ "trello-animated-alternative": isDarkContent })}
-        width={80}
-        height={80}
-      />
+      <Link
+        href="/boards"
+        className={clsx(
+          "rounded-sm p-2",
+          { "hover:bg-white/15": !isDarkContent },
+          { "hover:bg-black/15": isDarkContent }
+        )}
+      >
+        <Image
+          src="/assets/logos/trello-animated.gif"
+          alt="Trello logo"
+          className={clsx({ "trello-animated-alternative": isDarkContent })}
+          width={80}
+          height={80}
+        />
+      </Link>
     </header>
   );
 };
