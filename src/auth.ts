fcce492f67947ad05a752fdccb7e3363c9126006
juanpmachadob <juanpmachadob@gmail.com>
2023-12-16 17:21:20 -0500

    feat: add prisma adapter to authentication

--- src/auth.ts
+++ src/auth.ts
@@ -1,39 +1,33 @@
 import NextAuth, { type NextAuthConfig } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcryptjs from "bcryptjs";
 import prisma from "@/lib/prisma";
 
-type Credentials = {
-  email: string;
-  password: string;
-};
-
 export const authConfig: NextAuthConfig = {
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
   pages: {
     signIn: "/auth/login",
     newUser: "/auth/signup",
   },
-
   providers: [
     GoogleProvider,
     GithubProvider,
     CredentialsProvider({
-      credentials: {
-        email: { type: "text" },
-        password: { type: "text" },
-      },
       async authorize(credentials) {
-        const { email, password } = credentials as Credentials;
+        const { email, password } = credentials;
 
         const user = await prisma.user.findUnique({
-          where: { email: email.toLowerCase() },
+          where: { email: (email as string).toLowerCase() },
         });
         if (!user) return null;
 
         // Compare hashed password
-        if (!bcryptjs.compareSync(password, user.password)) return null;
+        if (!bcryptjs.compareSync(password as string, user.password || ""))
+          return null;
 
         // Return user without password
         const { password: _, ...rest } = user;
