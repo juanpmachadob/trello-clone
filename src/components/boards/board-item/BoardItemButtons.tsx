c98a3799b73b12678e8c27b9430a2e1678d69c3d
juanpmachadob <juanpmachadob@gmail.com>
2023-12-20 20:20:09 -0500

    feat: add optimistic updates for toggling board favorite status in "BoardItemButtons" component

--- src/components/boards/board-item/BoardItemButtons.tsx
+++ src/components/boards/board-item/BoardItemButtons.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { startTransition, useOptimistic } from "react";
 import { IoStar, IoStarOutline } from "react-icons/io5";
 import { toggleBoardFavorite } from "@/actions";
 import type { Board } from "@/interfaces";
@@ -8,20 +9,35 @@ interface Props {
 }
 
 export const BoardItemButtons = ({ board }: Props) => {
-  const handleToggleBoardFavorite = () => {
-    toggleBoardFavorite(board.id, !board.isFavorite);
+  const [boardOptimistic, toggleBoardOptimistic] = useOptimistic(
+    board,
+    (state, newFavoriteValue: boolean) => ({
+      ...state,
+      isFavorite: newFavoriteValue,
+    })
+  );
+
+  const handleToggleBoardFavorite = async () => {
+    const originalFavoriteStatus = boardOptimistic.isFavorite;
+    startTransition(() => toggleBoardOptimistic(!originalFavoriteStatus));
+
+    try {
+      await toggleBoardFavorite(boardOptimistic.id, !originalFavoriteStatus);
+    } catch (error) {
+      startTransition(() => toggleBoardOptimistic(originalFavoriteStatus));
+    }
   };
 
   return (
     <button className="absolute bottom-0 right-0 m-3 text-white duration-150 hover:scale-110">
-      {board.isFavorite && (
+      {boardOptimistic.isFavorite && (
         <IoStar
           size={16}
           className="text-[#e2b203]"
           onClick={handleToggleBoardFavorite}
         />
       )}
-      {!board.isFavorite && (
+      {!boardOptimistic.isFavorite && (
         <IoStarOutline size={16} onClick={handleToggleBoardFavorite} />
       )}
     </button>
