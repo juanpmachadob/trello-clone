8ab71130600301e72ae458ec70808dae36de100b
juanpmachadob <juanpmachadob@gmail.com>
2024-01-11 21:39:33 -0500

    feat: update 'listsOrder' field in board with removed list ID upon list deletion

--- src/actions/lists/delete-list.ts
+++ src/actions/lists/delete-list.ts
@@ -6,7 +6,8 @@ import prisma from "@/lib/prisma";
 export const deleteList = async (boardId: string, listId: string) => {
   const session = await auth();
 
-  if (!session?.user?.id) {
+  const userId = session?.user?.id as string;
+  if (!userId) {
     return {
       ok: false,
       data: null,
@@ -14,17 +15,47 @@ export const deleteList = async (boardId: string, listId: string) => {
     };
   }
 
-  return prisma.list
-    .delete({
-      where: {
-        id: listId,
-        board: {
-          id: boardId,
-          userId: session.user.id,
+  return prisma
+    .$transaction(async (tx) => {
+      // First, delete the list and select the board's listsOrder
+      const deletedList = await tx.list.delete({
+        where: {
+          id: listId,
+          board: { id: boardId, userId },
         },
-      },
+        select: {
+          board: {
+            select: {
+              listsOrder: true,
+            },
+          },
+        },
+      });
+
+      // Filter out the deleted list from the board's listsOrder
+      const newListsOrder = deletedList.board.listsOrder.filter(
+        (id) => id !== listId
+      );
+
+      // Then, update the board to remove the deleted list from the listsOrder array
+      await tx.board.update({
+        where: { id: boardId, userId },
+        data: {
+          listsOrder: newListsOrder,
+        },
+      });
     })
-    .then(() => {
+    .then(() => ({
+      ok: true,
+    }))
+    .catch((error) => {
+      console.error(error);
+      return {
+        ok: false,
+        error: "Internal server error",
+      };
+    })
+    .finally(() => {
       revalidatePath(`/boards/${boardId}`);
     });
 };
