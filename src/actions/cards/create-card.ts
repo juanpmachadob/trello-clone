3f1b2b2299fb72d06061f25c2e2d69d6f9f54d11
juanpmachadob <juanpmachadob@gmail.com>
2024-01-12 22:04:03 -0500

    feat: add createCard action to create a new card in a list

---
+++ src/actions/cards/create-card.ts
@@ -0,0 +1,54 @@
+"use server";
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+export const createCard = async (
+  boardId: string,
+  listId: string,
+  title: string
+) => {
+  const session = await auth();
+
+  const userId = session?.user?.id as string;
+  if (!userId) {
+    return {
+      ok: false,
+      error: "You must be logged in to create a card.",
+    };
+  }
+
+  return prisma
+    .$transaction(async (tx) => {
+      // First, create the card
+      const cardData = await tx.card.create({
+        data: {
+          title,
+          listId,
+        },
+      });
+
+      // Then, update the list to include the new card in the cardsOrder array
+      await tx.list.update({
+        where: { id: listId },
+        data: {
+          cardsOrder: {
+            push: cardData.id,
+          },
+        },
+      });
+    })
+    .then(() => ({
+      ok: true,
+    }))
+    .catch((error) => {
+      console.error(error);
+      return {
+        ok: false,
+        error: "Internal server error",
+      };
+    })
+    .finally(() => {
+      revalidatePath(`/boards/${boardId}`);
+    });
+};
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,4 @@
 export * from "./auth";
 export * from "./boards";
 export * from "./lists";
+export * from "./cards";
