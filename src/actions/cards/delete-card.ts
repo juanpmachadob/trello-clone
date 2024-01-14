06b0bc4ab674f959841752effc3ec8c2f46a898b
juanpmachadob <juanpmachadob@gmail.com>
2024-01-14 15:14:42 -0500

    feat: Add deleteCard action to delete a card from a list

---
+++ src/actions/cards/delete-card.ts
@@ -0,0 +1,65 @@
+"use server";
+import { revalidatePath } from "next/cache";
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
+
+export const deleteCard = async (
+  boardId: string,
+  listId: string,
+  cardId: string
+) => {
+  const session = await auth();
+
+  const userId = session?.user?.id as string;
+  if (!userId) {
+    return {
+      ok: false,
+      data: null,
+      error: "You must be logged in to delete a card.",
+    };
+  }
+
+  return prisma
+    .$transaction(async (tx) => {
+      // First, delete the card and select the list's cardsOrder
+      const deletedCard = await tx.card.delete({
+        where: {
+          id: cardId,
+          list: { id: listId, board: { id: boardId, userId } },
+        },
+        select: {
+          list: {
+            select: {
+              cardsOrder: true,
+            },
+          },
+        },
+      });
+
+      // Filter out the deleted card from the list's cardsOrder
+      const newCardsOrder = deletedCard.list.cardsOrder.filter(
+        (id) => id !== cardId
+      );
+
+      // Then, update the list to remove the deleted card from the cardsOrder array
+      await tx.list.update({
+        where: { id: listId },
+        data: {
+          cardsOrder: newCardsOrder,
+        },
+      });
+    })
+    .then(() => ({
+      ok: true,
+    }))
+    .catch((error) => {
+      console.error(error);
+      return {
+        ok: false,
+        error: "Internal server error",
+      };
+    })
+    .finally(() => {
+      revalidatePath(`/boards/${boardId}`);
+    });
+};
--- src/actions/cards/index.ts
+++ src/actions/cards/index.ts
@@ -1,1 +1,2 @@
 export * from "./create-card";
+export * from "./delete-card";
