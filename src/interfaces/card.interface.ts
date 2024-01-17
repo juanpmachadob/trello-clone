0a1ec4abf81f8132cd63e519a88f637d55808c51
juanpmachadob <juanpmachadob@gmail.com>
2024-01-16 21:00:46 -0500

    refactor: Add CardWithList interface to include list information

--- src/interfaces/card.interface.ts
+++ src/interfaces/card.interface.ts
@@ -1,10 +1,16 @@
+import { List } from "@/interfaces";
+
 export interface Card {
   id: string;
   title: string;
   description: string;
-  
+
   createdAt: Date;
   updatedAt: Date;
 
   listId: string;
+}
+
+export interface CardWithList extends Card {
+  list: Omit<List, "cards">;
 }
