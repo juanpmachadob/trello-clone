b3c573eea2561f4f4014f4c55a641a7c2c47473f
juanpmachadob <juanpmachadob@gmail.com>
2024-01-20 21:56:08 -0500

    refactor: Remove card ID from URL query params when closing popup

--- src/components/cards/card-popup/CardPopupHeader.tsx
+++ src/components/cards/card-popup/CardPopupHeader.tsx
@@ -1,9 +1,22 @@
 "use client";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { LuAppWindow, LuX } from "react-icons/lu";
 import { ButtonIcon, EditableText } from "@/components/ui";
 
 const CardPopupHeader = () => {
-  const handleClosePopup = () => {};
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  /**
+   * Removes the card ID from the URL query params to close the card popup
+   */
+  const handleClosePopup = () => {
+    const params = new URLSearchParams(searchParams);
+    params.delete("card");
+
+    router.push(`${pathname}?${params.toString()}`);
+  };
 
   return (
     <header className="mb-8 flex flex-row justify-between">
