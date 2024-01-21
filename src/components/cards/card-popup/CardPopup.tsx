e5e36e08099529292ac78d21239249f2e38cd37d
juanpmachadob <juanpmachadob@gmail.com>
2024-01-20 21:56:30 -0500

    feat: Fetch selected card in CardPopup component

--- src/components/cards/card-popup/CardPopup.tsx
+++ src/components/cards/card-popup/CardPopup.tsx
@@ -1,13 +1,37 @@
 "use client";
+import { useCallback, useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
+import { getCardById } from "@/actions";
 import CardPopupHeader from "./CardPopupHeader";
 import CardPopupBody from "./CardPopupBody";
 import CardPopupActions from "./CardPopupActions";
+import type { CardWithList } from "@/interfaces";
 
 const CardPopup = () => {
   const searchParams = useSearchParams();
+  const [cardWithList, setCardWithList] = useState<CardWithList>();
 
-  if (!searchParams.has("card")) return null;
+  /**
+   * Fetches the selected card from the URL query params
+   */
+  const getSelectedCard = useCallback(async () => {
+    const cardId = searchParams.get("card");
+    if (!cardId) return;
+
+    const { ok, data: cardWithList } = await getCardById(cardId);
+    if (!ok || !cardWithList) return;
+
+    setCardWithList(cardWithList);
+  }, [searchParams]);
+
+  /**
+   * Hook to fetch the selected card
+   */
+  useEffect(() => {
+    getSelectedCard();
+  }, [searchParams, getSelectedCard]);
+
+  if (!searchParams.has("card") || !cardWithList) return null;
   return (
     <div className="absolute inset-0 z-10 flex items-start justify-center bg-black/70">
       <section className="relative my-12 min-h-96 w-1/2 rounded-xl bg-background p-4">
